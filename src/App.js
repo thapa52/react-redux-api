0979af67cb16bd9c473c1bb3f63d5e5fb628f0e1
“thapa52” <thapa.pradeepp@gmail.comgit config --global user.name thapa52>
2022-05-23 11:04:03 +0545

    added route and router in app.js and added browser router and provider in index.js and udated productreducers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
+import ProductDetail from "./components/ProductDetail";
+import ProductLists from "./components/ProductLists";
+
 
 function App() {
   return (
     <div className="App">
-  <h1>Hello</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ProductLists />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
--- src/redux/reducers/productReducers.js
+++ src/redux/reducers/productReducers.js
@@ -1,17 +1,13 @@
 import { ActionTypes } from "../constants/action-types";
 
-const initialState = [
-  {
-    id: 1,
-    title: "thapa",
-    category: "category",
-  },
-];
+const initialState = {
+  products: [],
+};
 
 export const productReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
-      return state;
+      return { ...state, products: payload};
 
     default:
       return state;
